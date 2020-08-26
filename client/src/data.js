dc06d4be97210573ddf9a1a10a34d1ca67fe82f1
tarun dixit <dixittarun81@gmail.com>
2020-08-26 16:30:14 +0530

    dynamic rendering of products

---
+++ client/src/data.js
@@ -0,0 +1,52 @@
+export default {
+    products: [
+        {
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Tommy Hilfiger',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Tommy Hilfiger',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        }
+    ]
+}
