72230fae376551e69a7b7f12f94c7df0928305e4
tarun dixit <dixittarun81@gmail.com>
2020-08-27 22:33:42 +0530

    server setup

---
+++ server/data.js
@@ -0,0 +1,54 @@
+ data = {
+    products: [
+        {   _id: '1',
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {   _id: '2',
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Tommy Hilfiger',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {   _id: '3',
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {   _id: '4',
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Tommy Hilfiger',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {   _id: '5',
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {   _id: '6',
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        }
+    ]
+}
+
+module.exports = data;
